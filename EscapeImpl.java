d991b9b808662ceee01fd3d297042d412b1ab2fa
白宇晗 <baiyuhan@baiyuhandembp.lan>
2023-10-03 17:04:37 -0230

    add annotations

--- EscapeImpl.java
+++ EscapeImpl.java
@@ -1,60 +1,62 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class EscapeImpl implements Escape {
     /**
-     * Find a path from the entrance to the exit in a maze.
+     * Using DFS find a path from the entrance to the exit in a maze.
+     *
+     * @param maze Requires a valid Two-dimensional array set of type char.
+     * @return The result must be a continuous collection of valid two-dimensional arrays, or an empty collection if there is no path.
+     * @ensures myPerfectRecursionMethod() always return and no exception thrown, no side effect.
      */
     @Override
     public List<Pair<Integer, Integer>> escape(final char[][] maze) {
-        final List<Pair<Integer, Integer>> s = new ArrayList<>();
-        boolean enter = false;
-        boolean end = false;
-        int[] start = new int[2];
-        int[] destination = new int[2];
+        int[] start;
         for (int i = 0; i < maze.length; i++) {
             for (int j = 0; j < maze[0].length; j++) {
                 if (maze[i][j] == 'E') {
-                    enter = true;
                     start = new int[]{i, j};
-                    s.add(new Pair<>(i, j));
-
-
-                    // found the entrance
-
-                    // TODO: Your magic here...
-                    // blah, blah, blah...
-                }
-                if (maze[i][j] == 'X') {
-                    end = true;
-                    destination = new int[]{i, j};
+                    return myPerfectRecursionMethod(maze, start);
                 }
             }
-        }
-        if (enter && end) {
-            return myPerfectRecursionMethod(maze, start);
         }
         return new ArrayList<>();
     }
 
+    /**
+     * DFS function use recursion.
+     *
+     * @param maze  Requires a valid two-dimensional array of type char.
+     * @param start Requires an array of type int is required and cannot be empty.
+     * @return The result must be a continuous collection of valid two-dimensional arrays, or an empty collection if there is no path.
+     * @ensures dfs() always return and no exception thrown, no side effect.
+     */
     private List<Pair<Integer, Integer>> myPerfectRecursionMethod(char[][] maze, int[] start) {
         final List<Pair<Integer, Integer>> result = new ArrayList<>();
-        //有无访问过
+        //visit or not?
         boolean[][] visit = new boolean[maze.length][maze[0].length];
         dfs(maze, start, visit, result);
         return result;
     }
 
 
+    /**
+     * Recursive field
+     *
+     * @param maze   Requires a valid two-dimensional array of type char.
+     * @param start  Requires an array of type int is required and cannot be empty.
+     * @param visit  Requires a two-dimensional array of type boolean.
+     * @param result A list of custom types.
+     * @return result list will be returned.
+     * @ensures Always return and no exception thrown, no side effect.
+     */
     public boolean dfs(char[][] maze, int[] start, boolean[][] visit, List<Pair<Integer, Integer>> result) {
         if (maze[start[0]][start[1]] == 'X') {
             result.add(new Pair<>(start[0], start[1]));
             return true;
         }
         visit[start[0]][start[1]] = true;
-        //顺时针试探
-        //右
+        //right
         if (0 <= start[0] && start[0] < maze.length && 0 <= start[1] + 1 && start[1] + 1 < maze[0].length) {
             if (maze[start[0]][start[1] + 1] != 'W' && visit[start[0]][start[1] + 1] == false) {
                 visit[start[0]][start[1] + 1] = true;
@@ -64,7 +66,7 @@ public class EscapeImpl implements Escape {
                 }
             }
         }
-        //下
+        //down
         if (0 <= start[0] + 1 && start[0] + 1 < maze.length && 0 <= start[1] && start[1] < maze[0].length) {
             if (maze[start[0] + 1][start[1]] != 'W' && visit[start[0] + 1][start[1]] == false) {
                 visit[start[0] + 1][start[1]] = true;
@@ -74,7 +76,7 @@ public class EscapeImpl implements Escape {
                 }
             }
         }
-        //左
+        //left
         if (0 <= start[0] && start[0] < maze.length && 0 <= start[1] - 1 && start[1] - 1 < maze[0].length) {
             if (maze[start[0]][start[1] - 1] != 'W' && visit[start[0]][start[1] - 1] == false) {
                 visit[start[0]][start[1] - 1] = true;
@@ -84,7 +86,7 @@ public class EscapeImpl implements Escape {
                 }
             }
         }
-        //上
+        //up
         if (0 <= start[0] - 1 && start[0] - 1 < maze.length && 0 <= start[1] && start[1] < maze[0].length) {
             if (maze[start[0] - 1][start[1]] != 'W' && visit[start[0] - 1][start[1]] == false) {
                 visit[start[0] - 1][start[1]] = true;
